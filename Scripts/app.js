92cfbb675c281a88ccc5ff2afd72749faca92529
Esteban Arrangoiz <esteban.arrangoiz@gmail.com>
2021-11-24 11:24:11 -0500

    added basic files

---
+++ Scripts/app.js
@@ -0,0 +1,17 @@
+/* custom scripts go here */
+
+
+
+(function()
+{
+
+	// initialization function
+	function Start()
+	{
+		console.log("App started...");
+	}
+
+	// event listener, when the window has finished loading, it calls the Start function
+	window.addEventListener("load", Start)
+
+})();
