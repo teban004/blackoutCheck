2619506ea1d49e027bb90919e0e30bd5405c94e2
Esteban Arrangoiz <esteban.arrangoiz@gmail.com>
2021-11-24 23:39:35 -0500

    added check button and hability for it to execute JavaScript

--- Scripts/app.js
+++ Scripts/app.js
@@ -8,7 +8,19 @@
 	// initialization function
 	function Start()
 	{
+		// sending this message on the console just to know that this file has been loaded correctly and it's executing JavaScript
 		console.log("App started...");
+
+		// get a reference to the HTML element with id=checkButton
+		let checkButton = document.getElementById("checkButton");
+
+		// add a callback function to the button when it's clicked
+		checkButton.addEventListener("click", () => {
+			// the functionality of the button goes here
+			console.log("Check button was clicked!");
+		});
+			
+		
 	}
 
 	// event listener, when the window has finished loading, it calls the Start function
