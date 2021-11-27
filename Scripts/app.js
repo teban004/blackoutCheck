ffa95ede424830ce7732a2c615aa5c07ee50cf05
Esteban Arrangoiz <esteban.arrangoiz@gmail.com>
2021-11-27 15:21:20 -0500

    fixed Postal Code validation

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,7 +1,5 @@
 /* custom scripts go here */
 
-
-
 (function()
 {
 
@@ -22,17 +20,34 @@
 
 			let result = document.getElementById("result");
 
+			let team = document.getElementById("team").value;
+
 			// check if the user entered the 3 characters of the zip code
-			let zipCode = document.getElementById("zipCode").value;
+			let zipCode = document.getElementById("zipCode").value.toUpperCase();
+
+			let validZipCode = function(zipCode) {
+				let regex = new RegExp(/^[ABCEGHJKLMNPRSTVXY]\d[ABCEGHJKLMNPRSTVWXYZ]$/i);
+				if (regex.test(zipCode))
+					return true;
+				else
+					return false;
+			};
+
 			if (zipCode.length < 3) {
 				result.className = "alert alert-danger";
-				result.innerHTML = "<p>Please enter the first characters of the zip code.</p>";
+				result.innerHTML = "<p>Please enter the first 3 characters of the zip code.</p>";
 			}
 			else {
-				let brand = document.querySelector('input[name="brand"]:checked').id;
-				
-				result.className = "alert alert-info";
-				result.innerHTML = "<p>The result goes here!</p><p>The selected brand is: <b>" + brand + "</b></p><p>The zip code is: <b>" + zipCode + "</b></p>";
+				if ( validZipCode(zipCode) ) {
+					let brand = document.querySelector('input[name="brand"]:checked').id;
+					
+					result.className = "alert alert-info";
+					result.innerHTML = "<p>The result goes here!</p><p>The selected brand is: <b>" + brand + "</b></p><p>The zip code is: <b>" + zipCode + "</b></p><p>The selected team is: <b>" + team + "</b></p><p>Zip code validation: <b>" + validZipCode(zipCode) + "</b></p>";
+				}
+				else {
+					result.className = "alert alert-danger";
+					result.innerHTML = "<p>The zip code <b>" + zipCode + "</b> is not valid.</p>";
+				}
 			}
 		});
 			
